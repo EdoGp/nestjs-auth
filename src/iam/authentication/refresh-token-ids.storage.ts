f10e41d26804a07d03569e9858576140239f0baf
Eduardo Gonzalez <eduardgp@gmail.com>
2023-03-10 08:51:54 +0100

    feat: temporary disable redis

--- src/iam/authentication/refresh-token-ids.storage.ts
+++ src/iam/authentication/refresh-token-ids.storage.ts
@@ -3,7 +3,7 @@ import {
   OnApplicationBootstrap,
   OnApplicationShutdown,
 } from '@nestjs/common';
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 
 export class InvalidatedRefreshTokenError extends Error {}
 
@@ -14,27 +14,28 @@ export class RefreshTokenIdsStorage
   private redisClient: Redis;
 
   onApplicationBootstrap() {
-    this.redisClient = new Redis({ host: 'localhost', port: 6379 });
+    // this.redisClient = new Redis({ host: 'localhost', port: 6379 });
   }
   onApplicationShutdown(signal?: string) {
-    this.redisClient.quit();
-    throw new Error('Method not implemented.');
+    // this.redisClient.quit();
+    // throw new Error('Method not implemented.');
   }
 
   async insert(userId: string, tokenId: string): Promise<void> {
-    await this.redisClient.set(this.getKey(userId), tokenId);
+    // await this.redisClient.set(this.getKey(userId), tokenId);
   }
 
   async validate(userId: string, tokenId: string): Promise<boolean> {
-    const storedId = await this.redisClient.get(this.getKey(userId));
-    if (storedId !== tokenId) {
-      throw new InvalidatedRefreshTokenError();
-    }
-    return storedId === tokenId;
+    // const storedId = await this.redisClient.get(this.getKey(userId));
+    // if (storedId !== tokenId) {
+    //   throw new InvalidatedRefreshTokenError();
+    // }
+    // return storedId === tokenId;
+    return true;
   }
 
   async invalidate(userId: string): Promise<void> {
-    await this.redisClient.del(this.getKey(userId));
+    // await this.redisClient.del(this.getKey(userId));
   }
 
   private getKey(userId: string): string {
