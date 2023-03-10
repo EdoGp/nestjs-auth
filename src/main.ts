bc410dd4bf45b000d9d47188ea732674e6ba1241
Eduardo Gonzalez <eduardgp@gmail.com>
2023-03-10 09:31:08 +0100

    feat: added modules

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors();
-  // app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(helmet());
   const options = new DocumentBuilder()
     .setTitle('Basic Nestjs CRUD API with Nextjs')
     .setDescription('Basic nestjs CRUD API with users and authentication')
@@ -15,7 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document, {
     customSiteTitle: 'Basic Nestjs CRUD API',
-    // customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
