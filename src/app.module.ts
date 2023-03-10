4496eb7dd2527e3b7b4e5c689651fc8155865503
Eduardo Gonzalez <eduardgp@gmail.com>
2023-03-10 09:13:32 +0100

    feat: testing access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { ConfigModule } from '@nestjs/config';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { IamModule } from './iam/iam.module';
-import { UsersModule } from './users/users.module';
+// import { IamModule } from './iam/iam.module';
+// import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    UsersModule,
-    IamModule,
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    // ConfigModule.forRoot(),
+    // UsersModule,
+    // IamModule,
+    // MongooseModule.forRoot(process.env.MONGO_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// import * as helmet from 'helmet';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  // app.use(helmet());
+  app.use(helmet());
   const options = new DocumentBuilder()
     .setTitle('Basic Nestjs CRUD API')
     .setDescription('Basic nestjs CRUD API with users and authentication')
-    .setVersion(process.env.version || '0.1.1beta')
+    .setVersion(process.env.version || '0.0.1beta')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
