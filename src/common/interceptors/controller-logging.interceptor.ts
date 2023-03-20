2e5c0435d9892ba71e401ae131f68b08b5a7d907
Eduardo Gonzalez <eduardgp@gmail.com>
2023-03-20 15:16:23 +0100

    feat: updated cookie parser

--- src/common/interceptors/controller-logging.interceptor.ts
+++ src/common/interceptors/controller-logging.interceptor.ts
@@ -2,11 +2,11 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
-  NestInterceptor
+  NestInterceptor,
 } from '@nestjs/common';
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import {CustomLogger} from './../logger.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CustomLogger } from './../logger.service';
 
 @Injectable()
 export class ControllerLoggingInterceptor implements NestInterceptor {
@@ -18,9 +18,6 @@ export class ControllerLoggingInterceptor implements NestInterceptor {
     const { statusCode } = context.getArgByIndex(1);
     this.logger.verbose(
       `[INC][${method}] ${url}
-      ${
-        // cookies && Object.keys(cookies).length ? ' [COOKIES][✓]' : ''
-      }
       ${Object.keys(body).length ? ` [BODY] ${JSON.stringify(body)}` : ''}${
         Object.keys(params).length ? ` [PARAMS] ${JSON.stringify(params)}` : ''
       }${Object.keys(query).length ? ` [QUERY] ${JSON.stringify(query)}` : ''}`,
